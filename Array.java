316ae90d5be0758d048630401cbd21d5ab15eca8
mohitbharde <mohitbharde@gmail.com>
2024-01-17 13:32:23 +0530

    added array.java

---
+++ Array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Array{
+
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+
+        int mark[]= new int[50];
+
+        mark[0]=60;
+        mark[1]=40;
+        mark[6]=80;
+
+        System.out.println("mark at 0 index "+ mark[0]);
+        System.out.println("mark at 1 index "+ mark[1]);
+        System.out.println("mark at 6 index "+ mark[6]);
+    }
+}
