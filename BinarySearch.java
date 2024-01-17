7b3a098ec644c6377885a136b6186f45382dd2ad
mohitbharde <mohitbharde@gmail.com>
2024-01-17 21:20:06 +0530

    added binary search algorithm

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+
+    // Binary Search
+    static int binarySearch(int numbers[], int key) {
+        int start = 0, end = numbers.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (numbers[mid] == key) {
+                return mid;
+            }
+            if (numbers[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 2, 4, 6, 8, 10, 12, 14 };
+        int key = 21;
+        System.out.println("Element found at index: " + binarySearch(numbers, key));
+    }
+}
