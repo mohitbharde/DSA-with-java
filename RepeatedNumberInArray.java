db82e92f0d975ec0fe0a57703ca65c259907e65c
mohitbharde <mohitbharde@gmail.com>
2024-01-20 17:18:30 +0530

    add new file RepeatedNumberArray

---
+++ RepeatedNumberInArray.java
@@ -0,0 +1,17 @@
+public class RepeatedNumberInArray {
+    public static boolean repeatedNumber(int number[]) {
+        int n = number.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (number[i] == number[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int number[] = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+        System.out.println("Repeated Number is : " + repeatedNumber(number));
+    }
+}
